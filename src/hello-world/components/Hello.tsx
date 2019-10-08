78208b7ebfd330534f051f2ecbdcdec6cab5041c
Daniel Carr <d.douglas.carr@gmail.com>
2019-10-09 09:48:47 +1100

    styled components

--- src/hello-world/components/Hello.tsx
+++ src/hello-world/components/Hello.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const HelloDiv = styled.div`
+  padding: 10px;
+  color: blue;
+`;
 
 export class Hello extends React.Component<{}, {}> {
   render() {
-    return <div>Hello Reloadable World</div>
+    return <HelloDiv>Hello Reloadable World</HelloDiv>
   }
-}
+};
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
+const styledComponentsTransformer = createStyledComponentsTransformer();
+
+
 module.exports = {
   entry: './src/index.tsx',
   mode: isProd ? 'production' : 'development',
@@ -13,7 +17,10 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        loader: 'ts-loader',
+        options: {
+          getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
+        },
         exclude: /node_modules/,
       },
     ],
